ef103547749ab1dad2c95985f8f9262391be215e
DominikaPietryka <dominika.pietryka@gmail.com>
2024-11-27 16:16:50 +0100

    wersje

--- 02/app.js
+++ 02/app.js
@@ -1,3 +1,4 @@
 console.log('Działa! Gratuluję uruchomienia JS z poziomu Node.js!');
 console.log('Moja wersja Node.js to: [tutaj wpisz swoją wersję]');
-console.log('Moja wersja npm to: [tutaj wpisz swoją wersję]');
+console.log('Moja wersja node to: [20.18.0]');
+console.log('Moja wersja npm to: [10.9.0]');
